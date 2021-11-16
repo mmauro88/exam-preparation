c06410183a2cac0ff14dae550b9a02dbbd3fe78b
Mauro1919 <>
2021-11-16 10:56:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+# Question 16(a)
+# Examples Number:
+#num1 = 9
+#num2 = 5
+
+#print ("Do you want me to (a)dd or (s)ubstract?)"
+#choice = input()
+
+##if choice =="a"
+# print(num1 + num2)
+# elif choice == "s":
+  # print(num1-num2)
+  # choice=a
+####################################################################################################################
+#test
+
+#### Describe each datatype below:(4 marks)
+
+## 4         = integer
+## 5.7       = float
+## True      = boolean
+## Good Luck = string
+
+
+#### Which datatype would be useful for storing someone's height? (1 mark)
+## Answer
+
+#float
+
+
+#### Which datatype would be useful for storing someone's hair colour?(1 mark)
+## Answer
+
+#string
+
+####Create a variable tha will store the users name.(1 mark)
+
+#input("please enter your name:")
+
+
+####Create a print statement that will print the first 4 characters of the person's name.(3 marks)
+
+#print("the first 4 characters of your name")
+
+
+####Convert the user's name to all uppercase characters and print the result
+
+#username= "Mauro"
+#print(username.upper)
+
+
+####Find out how many times the letter A occurs in the user's name
+
+#username="Mauro"
+#print(username.count("a"))
+
+
+#### Create a conditional statement to ask a user to enter their age. If they are older than 18 they receive a message saying they can enter the competition, if they are under 18, they receive a message saying they cannot enter the competition.
+
+#age=int(input("Enter your age:"))
+#if age>18:
+#  print("you can enter competition")
+#else:
+#  print("you can't enter the competition")
+
+
+#### Create an empty list called squareNumbers (3 marks)
+
+#numlist=["squareNumbers"]
+
+#### Square numbers are the solutions to a number being multiplied by itself( example 1 is a square number because 1 X 1 = 1, 4 is a square number because 2 X 2 = 4)
+
+#squ
+
+###Calculate the first 20 square numbers and put them in the list called squareNumbers. (With loop and .append 10 marks, without, Max 6 marks).
+
+count -1
+while count<21:
+ num-count*count 
+ squareNumbers.append(num1)
+
+####Print your list (1 mark)
+
+####Create a variable called userSquare that asks the user for their favourite square number
+
+#userSquare= 0
+#print("Enter your favourite squareNumbers")
+
+#### Add this variable to the end of your list called SquareNumbers
+
+### Create a variable called choice. This variable should choose a random element from your list. Print the variable called choice.(3 marks)
+
+
+####Create another print statement that prints tha following output: The random square number is: XX, where XX is where the random square number chosen by the computer.(4 marks)
